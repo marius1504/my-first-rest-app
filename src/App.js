cdb642cd47a4ac390201095192c1579fb22ce73f
nichi <nihciteleamarius@gmail.com>
2022-05-02 20:59:00 +0300

    Tema Curs 23

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://media-exp1.licdn.com/dms/image/C4D0BAQFDno7AIQuw2Q/company-logo_200_200/0/1649524910351?e=2147483647&v=beta&t=ZcU1PS67ByQn29L5oHZr7HX556P7T3eKx9XkXKCO71I" className="App-logo" alt="logo" />
+
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome! This is my first React app!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.devmind.ro/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn with Devmind
         </a>
       </header>
     </div>
